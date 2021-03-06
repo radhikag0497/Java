5205eff690b4baf2289cc8cca9dd193edb787cbe
radhikag0497 <79258517+radhikag0497@users.noreply.github.com>
2021-03-06 14:09:46 +0530

    Update Roman_to_Int.java

--- Roman_to_Int.java
+++ Roman_to_Int.java
@@ -9,6 +9,10 @@
 // D             500
 // M             1000
 
+// I can be placed before V (5) and X (10) to make 4 and 9. 
+// X can be placed before L (50) and C (100) to make 40 and 90. 
+// C can be placed before D (500) and M (1000) to make 400 and 900.
+
 class Solution {
     public int romanToInt(String s) {
         int output=0;
